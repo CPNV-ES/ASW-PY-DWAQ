07b141ee5232aa0e7168869f1a9ff3e0ba7e4878
SuperQuentin <quentin.aellen@cpnv.ch>
2021-05-05 00:18:10 +0200

    refactor method name add test_ prefix

--- tests/vpc_unit_test.py
+++ tests/vpc_unit_test.py
@@ -1,32 +1,39 @@
 import unittest
+import asyncio
 
-from src.aws_vpc_manager import AwsVpcManager
+import src.aws_vpc_manager as aws_m
 import src.exception
 
 
 class UnitTestAwsVpcManager(unittest.IsolatedAsyncioTestCase):
-    """This test class validates the good behavior of the VpcManager class
+    """
+    This test class validates the good behavior of the VpcManager class
     """
 
     def setUp(self):
-        """Setup test vpc properties and instantiate the vpc manager
+        """
+        Setup test vpc properties and instantiate the vpc manager
         """
         self.__profile_name = "VIR1_INFRA_DEPLOYMENT"
         self.__region_end_point = "ap-south-1"
-        self.__vpc_manager = AwsVpcManager(self.__profile_name, self.__region_end_point)
+        self.__vpc_manager = aws_m.AwsVpcManager(self.__profile_name, self.__region_end_point)
         self.__vpc_tag_name = "VIR1_SCRUMMASTER"
         self.__cidr_block = "10.0.0.0/16"
 
-    async def create_vpc_nominal_case_success(self):
+    async def test_create_vpc_nominal_case_success(self):
+        """
+        This test method tests the vpc creation action.
+        This is the nominal case (all parameters are correctly set).
+        :return: The return type is mandatory when using async Task test method
+        """
         await self.__vpc_manager.create_vpc(self.__vpc_tag_name, self.__cidr_block)
         self.assertTrue(await self.__vpc_manager.exists(self.__vpc_tag_name))
 
-    async def create_vpc_already_exists_throw_exception(self):
+    async def test_create_vpc_already_exists_throw_exception(self):
         """
         This test method tests the vpc creation action.
         We expected the exception "VpcAlreadyExists".
-        :return:The return type is mandatory when using async Task test method
-
+        :return: The return type is mandatory when using async Task test method
         """
         # given
         await self.__vpc_manager.create_vpc(self.__vpc_tag_name, self.__cidr_block)
@@ -35,7 +42,7 @@ class UnitTestAwsVpcManager(unittest.IsolatedAsyncioTestCase):
             await self.__vpc_manager.create_vpc(self.__vpc_tag_name, self.__cidr_block)
         # then : Exception must be thrown
 
-    async def delete_vpc_nominal_case_success(self):
+    async def test_delete_vpc_nominal_case_success(self):
         """
         This test method tests the vpc deletion action.
         :return: The return type is mandatory when using async Task test method
@@ -47,7 +54,7 @@ class UnitTestAwsVpcManager(unittest.IsolatedAsyncioTestCase):
         # then
         self.assertFalse(await self.__vpc_manager.exists(self.__vpc_tag_name))
 
-    async def exists_vpc_nominal_case_success(self):
+    async def test_exists_vpc_nominal_case_success(self):
         """
         This test method tests the exist action.
         :return: The return type is mandatory when using async Task test method
@@ -59,7 +66,7 @@ class UnitTestAwsVpcManager(unittest.IsolatedAsyncioTestCase):
         # then
         self.assertTrue(result)
 
-    async def tearDown(self):
+    async def asyncTearDown(self):
         """
         This method is used to clean class properties after each test method
         """
