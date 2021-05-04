5c11570f95cedc3c572f618cecc2b069d03af0b0
SuperQuentin <quentin.aellen@cpnv.ch>
2021-05-05 01:46:08 +0200

    update basepath of doc generation oo the src folder

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os
+import inspect
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -6,13 +9,17 @@
 
 # -- Path setup --------------------------------------------------------------
 
+__location__ = os.path.join(
+    os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe()))
+)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.join(__location__, "../src"))
 
 
 # -- Project information -----------------------------------------------------
