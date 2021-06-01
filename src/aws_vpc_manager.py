dd8afde0de2844831b385e1e2851af95f08a5fd4
SuperQuentin <quentin.aellen@cpnv.ch>
2021-06-01 14:42:26 +0200

    refactor change client and ressource visiblity from public to protected

--- src/aws_vpc_manager.py
+++ src/aws_vpc_manager.py
@@ -7,8 +7,8 @@ import src.exception.vpc_exception as vpc_exception
 class AwsVpcManager(IVpcManager):
     def __init__(self):
         # AmazonEc2Client
-        self.client = boto3.client('ec2')
-        self.resource = boto3.resource('ec2')
+        self._client = boto3.client('ec2')
+        self._resource = boto3.resource('ec2')
         # Vpcs list
         self.vpcs = None
 
@@ -24,7 +24,7 @@ class AwsVpcManager(IVpcManager):
             The primary IPv4 CIDR block for the VPC
         """
         if not await self.exists(vpc_tag_name):
-            vpc = self.resource.create_vpc(CidrBlock=cidr_block)
+            vpc = self._resource.create_vpc(CidrBlock=cidr_block)
             vpc.create_tags(Tags=[{"Key": "Name", "Value": vpc_tag_name}])
             vpc.wait_until_available()
         else:
@@ -41,7 +41,7 @@ class AwsVpcManager(IVpcManager):
         """
         if await self.exists(vpc_tag_name):
             vpc_id = await self.vpc_id(vpc_tag_name)
-            self.client.delete_vpc(VpcId=vpc_id)
+            self._client.delete_vpc(VpcId=vpc_id)
         else:
             raise vpc_exception.VpcNameDoesntExists('AwsVpcManager', 'Vpc "' + vpc_tag_name + '" doesn\'t exists')
 
@@ -58,17 +58,17 @@ class AwsVpcManager(IVpcManager):
         -------
         Boolean : True if the vpc exists
         """
-        response = self.client.describe_vpcs(Filters=[{'Name': 'tag:Name', 'Values': [vpc_tag_name]}])
+        response = self._client.describe_vpcs(Filters=[{'Name': 'tag:Name', 'Values': [vpc_tag_name]}])
         return True if response['Vpcs'] else False
 
     async def describe_vpcs(self):
         """
         Retrieve all the vpcs
         """
-        vpcs = list(self.resource.vpcs.filter(Filters=[]))
+        vpcs = list(self._resource.vpcs.filter(Filters=[]))
         response = None
         for vpc in vpcs:
-            response = self.client.describe_vpcs(
+            response = self._client.describe_vpcs(
                 VpcIds=[
                     vpc.id,
                 ]
@@ -84,7 +84,7 @@ class AwsVpcManager(IVpcManager):
         vpc_tag_name : string
             The name of the vpc
         """
-        response = self.client.describe_vpcs(Filters=[{'Name': 'tag:Name', 'Values': [vpc_tag_name]}])
+        response = self._client.describe_vpcs(Filters=[{'Name': 'tag:Name', 'Values': [vpc_tag_name]}])
         if response['Vpcs']:
             return response['Vpcs'][0]['VpcId']
         raise vpc_exception.VpcNameDoesntExists('AwsVpcManager', 'Vpc "' + vpc_tag_name + '" doesn\'t exists')
