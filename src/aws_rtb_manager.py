384ed809ab4858702ea8a73b1043d7ae71132144
Hausmann <william.hausmann@cpnv.ch>
2021-06-08 12:32:27 +0200

    New exception (DoesntExists) & add comments for doxygen

--- src/aws_rtb_manager.py
+++ src/aws_rtb_manager.py
@@ -10,10 +10,16 @@ class AwsRtbManager(IRtbManager):
         self.client = boto3.client('ec2', use_ssl=False)
         self.resource = boto3.resource('ec2', use_ssl=False)
 
-
     async def create(self, rtb_tag_name, vpc_id):
         """
-        Create the specified route table associated to the vpc
+        Create the specified route table
+        @param rtb_tag_name: name of the route table
+        @type rtb_tag_name: str
+        @param vpc_id: the id of the vpc to attach
+        @type vpc_id: int
+        @return: none
+        @rtype: none
+        @raise: exception when the route table already exists
         """
         try:
             self.client.create_route_table(
@@ -33,7 +39,13 @@ class AwsRtbManager(IRtbManager):
 
     async def associate(self, rtb_id, subnet_id):
         """
-        Associate the specified route table with the subnet
+        Associate the specified route table to the specified subnet
+        @param rtb_id: id of the route table for the association
+        @type rtb_id: int
+        @param subnet_id: id of the subnet for the association
+        @type subnet_id: int
+        @return: none
+        @rtype: none
         """
         self.client.associate_route_table(
             RouteTableId=rtb_id,
@@ -42,7 +54,11 @@ class AwsRtbManager(IRtbManager):
 
     async def disassociate(self, association_id):
         """
-        Dissociate the specified route table
+        Disassociate the specified association
+        @param association_id: id of the association
+        @type association_id: int
+        @return: none
+        @rtype: none
         """
         self.client.disassociate_route_table(
             AssociationId=association_id,
@@ -50,7 +66,11 @@ class AwsRtbManager(IRtbManager):
 
     async def delete(self, rtb_id):
         """
-        Delete the specified route table
+        Delete the specified rtb
+        @param rtb_id: id of the route table for the deletion
+        @type rtb_id: int
+        @return: none
+        @rtype: none
         """
         self.client.delete_route_table(
             RouteTableId=rtb_id,
@@ -58,7 +78,15 @@ class AwsRtbManager(IRtbManager):
 
     async def create_route_igw(self, rtb_id, cidr_block, gateway_id):
         """
-        Create the specified route table with the internet gateway
+        Create route with a gateway
+        @param rtb_id: id of the route table to use it to create route
+        @type rtb_id: int
+        @param cidr_block: IPv4 CIDR block for the route
+        @type cidr_block: str
+        @param gateway_id: id of gateway to attach it to the route
+        @type gateway_id: int
+        @return: none
+        @rtype: none
         """
         self.client.create_route(
             DestinationCidrBlock=cidr_block,
@@ -68,7 +96,11 @@ class AwsRtbManager(IRtbManager):
 
     async def describe(self, rtb_tag_name):
         """
-        Describe the specified route table
+        Describe a specified route table using the name
+        @param rtb_tag_name: name of the route table
+        @type rtb_tag_name: str
+        @return: Route table properties
+        @rtype: array
         """
         return self.client.describe_route_tables(
             Filters=[
@@ -81,34 +113,53 @@ class AwsRtbManager(IRtbManager):
 
     async def exists(self, rtb_tag_name):
         """
-        Check if the specified route table exists
-        :return: Boolean
+        Check if the specified route table using the name exists
+        @param rtb_tag_name: name of the route table
+        @type rtb_tag_name: str
+        @return: Boolean
+        @rtype: bool
         """
         return True if await self.get_rtb_id(rtb_tag_name) else False
 
     async def get_assoc_id(self, rtb_tag_name):
         """
-        Get the specified route table association id
-        :return: Int
+        Get the association id
+        @param rtb_tag_name: name of the route table
+        @type rtb_tag_name: str
+        @return: Id of the association
+        @rtype: int
+        @raise: exception when the route table doesnt exists
         """
         response = await self.describe(rtb_tag_name)
         try:
             return response['RouteTables'][0]["Associations"][0]["RouteTableAssociationId"]
         except IndexError:
-            return None
+            raise rtb_exception.RtbDoesntExists
 
     async def get_rtb_id(self, rtb_tag_name):
         """
-        Get the specified route table id
-        :return: Int
+        Get the route table id
+        @param rtb_tag_name: name of the route table
+        @type rtb_tag_name: str
+        @return: Id of the route table
+        @rtype: int
+        @raise: exception when the route table doesnt exists
         """
         response = await self.describe(rtb_tag_name)
         try:
             return response['RouteTables'][0]["RouteTableId"]
         except IndexError:
-            return None
+            raise rtb_exception.RtbDoesntExists
 
     async def get_main_rtb_id_from_vpc(self, vpc_id):
+        """
+        Get the main route table id attached to a specified vpc
+        @param vpc_id: id of the vpc
+        @type vpc_id: int
+        @return: Id of the main route table
+        @rtype: int
+        @raise: exception when the route table doesnt exists
+        """
         main_route_table = self.client.describe_route_tables(
             Filters=[
                 {
@@ -122,4 +173,4 @@ class AwsRtbManager(IRtbManager):
         try:
             return main_route_table['RouteTables'][0]['Associations'][0]['RouteTableId']
         except IndexError:
-            return None
+            raise rtb_exception.RtbDoesntExists
--- src/exception/rtb_exception.py
+++ src/exception/rtb_exception.py
@@ -2,3 +2,9 @@ class RtbAlreadyExists(Exception):
     def __init__(self, expression="RtbAlreadyExists", message="Rtb already exists!"):
         self.expression = expression
         self.message = message
+
+
+class RtbDoesntExists(Exception):
+    def __init__(self, expression="RtbDoesntExists", message="Rtb Doesnt exists!"):
+        self.expression = expression
+        self.message = message
