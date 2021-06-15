8b0aff5393d5c27393c7b5600d25baa2cc79e14e
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-06-15 09:54:00 +0200

    Refactor main integration test

--- tests/integration_tests/main_integration_test.py
+++ tests/integration_tests/main_integration_test.py
@@ -5,8 +5,6 @@ import src.aws_rtb_manager as rtb_m
 import src.aws_internet_gateway_manager as igw_m
 import src.aws_subnet_manager as subnet_m
 
-import src.exception.rtb_exception as rtb_exception
-
 
 class IntegrationTestAwsRtbManager(unittest.IsolatedAsyncioTestCase):
 
@@ -32,26 +30,26 @@ class IntegrationTestAwsRtbManager(unittest.IsolatedAsyncioTestCase):
     async def test_scenario_nominal_case_success(self):
         # Create the VPC
         await self.__vpc_manager.create_vpc(self.__vpc_tag_name, "10.0.0.0/16")
-        self.__vpc_id = await self.__vpc_manager.vpc_id(self.__vpc_tag_name)
+        self.__vpc_id = await self.__vpc_manager.get_id(self.__vpc_tag_name)
         self.__main_rtb_id = await self.__rtb_manager.get_main_rtb_id_from_vpc(self.__vpc_id)
 
         # Create the public subnet "10.0.0.0/24"
         await self.__subnet_manager.create_subnet(self.__public_subnet_tag_name, "10.0.0.0/24", self.__vpc_id)
-        self.__public_subnet_id = await self.__subnet_manager.subnet_id(self.__public_subnet_tag_name)
+        self.__public_subnet_id = await self.__subnet_manager.get_id(self.__public_subnet_tag_name)
 
         # Create the private subnet "10.0.1.0/24"
         await self.__subnet_manager.create_subnet(self.__private_subnet_tag_name, "10.0.1.0/24", self.__vpc_id)
-        self.__private_subnet_id = await self.__subnet_manager.subnet_id(self.__private_subnet_tag_name)
+        self.__private_subnet_id = await self.__subnet_manager.get_id(self.__private_subnet_tag_name)
 
         # Create the Internet Gateway and attach it to the vpc
         await self.__igw_manager.create_internet_gateway(self.__igw_tag_name)
         # Attach Internet Gateway to vpc
         await self.__igw_manager.attach_to_vpc(self.__igw_tag_name, self.__vpc_tag_name)
-        self.__igw_id = await self.__igw_manager.internet_gateway_id(self.__igw_tag_name)
+        self.__igw_id = await self.__igw_manager.get_id(self.__igw_tag_name)
 
         # Create the route table for the internet gateway
         await self.__rtb_manager.create(self.__rtb_tag_name, self.__vpc_id)
-        self.__rtb_id = await self.__rtb_manager.get_rtb_id(self.__rtb_tag_name)
+        self.__rtb_id = await self.__rtb_manager.get_id(self.__rtb_tag_name)
         self.assertTrue(await self.__rtb_manager.exists(self.__rtb_tag_name))
 
         # Associate subnets to the corresponding routes tables
