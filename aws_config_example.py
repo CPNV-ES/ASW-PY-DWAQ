4d37d213a60dfe0078caa8cb786063b8a0d7476d
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-06-15 11:32:43 +0200

    Have an aws config file

---
+++ aws_config_example.py
@@ -0,0 +1,15 @@
+from botocore.config import Config
+
+aws = {
+    "aws_service_name": "",
+    "config": Config(
+        region_name="",
+        signature_version='v4',
+        retries={
+            'max_attempts': 10,
+            'mode': 'standard'
+        }
+    ),
+    "aws_access_key_id": "",
+    "aws_secret_access_key": "",
+}
--- src/aws_manager.py
+++ src/aws_manager.py
@@ -1,7 +1,18 @@
 import boto3
+import aws_config as cfg
 
 
 class AwsManager:
     def __init__(self):
-        self._client = boto3.client('ec2', use_ssl=False)
-        self._resource = boto3.resource('ec2', use_ssl=False)
+        self._client = boto3.client(
+            cfg.aws["aws_service_name"],
+            config=cfg.aws["config"],
+            aws_access_key_id=cfg.aws["aws_access_key_id"],
+            aws_secret_access_key=cfg.aws["aws_secret_access_key"],
+        )
+        self._resource = boto3.resource(
+            cfg.aws["aws_service_name"],
+            config=cfg.aws["config"],
+            aws_access_key_id=cfg.aws["aws_access_key_id"],
+            aws_secret_access_key=cfg.aws["aws_secret_access_key"],
+        )
